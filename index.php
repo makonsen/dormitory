0a0d6c6333ed0cbb02e599539b48b5db511ea9f5
natty <makonsennatthi@gmail.com>
2026-06-05 17:55:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-for ($i = 1; $i <= 100; $i++) {
-    if ($i % 2 === 0) {
-        echo "$i is even";
-    } else {
-        echo "$i is odd";
-    }
-    if ($i < 100) {
-        echo "\n";
-    }
-}
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 2 === 0) {
+//         echo "$i is even";
+//     } else {
+//         echo "$i is odd";
+//     }
+//     if ($i < 100) {
+//         echo "\n";
+//     }
+// }
+echo "test"
 ?>
